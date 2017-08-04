a6c0f938ce649f06343865dc1b836ff2837a3320
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-05 00:53:27 +0530

    Etag generation added

--- etags.go
+++ etags.go
@@ -1,8 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"time"
 )
+
+func genEtags(path string) error {
+	info, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if info.IsDir() {
+		files, _ := ioutil.ReadDir(path)
+		etags := make(map[string]string)
+		for _, file := range files {
+			if file.IsDir() {
+				if er := genEtags(filepath.Join(path, file.Name())); er != nil {
+					return er
+				}
+			} else if file.Name() == "etags.json" {
+				continue
+			} else {
+				etags[file.Name()] = tTag(file.ModTime())
+			}
+		}
+		etf, err := os.Create(filepath.Join(path, "etags.json"))
+		if err != nil {
+			return err
+		}
+		enc := json.NewEncoder(etf)
+		enc.SetIndent("", " ")
+		enc.Encode(etags)
+		etf.Close()
+		return nil
+	}
+	return errors.New("Not a directory")
+}
 
 func tTag(t time.Time) string {
 	buf := make([]byte, 3, 3)
