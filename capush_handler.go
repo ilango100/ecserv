b30129a20f5253c8e912d1ce6ae43482dafc8608
Ilango Rajagopal <ilangokul@gmail.com>
2017-09-06 19:03:12 +0530

    capush: push handler defined

--- capush_handler.go
+++ capush_handler.go
@@ -3,9 +3,41 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func createHandler() http.Handler {
+type CAPHandler struct {
+	etags map[string]string
+	deps  map[string][]string
+}
 
+func (c *CAPHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	//search for etag
+	etag, found := c.etags[req.URL.Path[1:]]
+
+	//not found, send not found
+	if !found {
+		rw.WriteHeader(404)
+		rw.Write([]byte("File not found!"))
+
+	} else /*found*/ {
+		rw.Write([]byte(etag))
+	}
+}
+
+func createHandler() http.Handler {
+	ets, err := depEtags(set.Root)
+	if err != nil {
+		fmt.Println("Error generating etags... Falling back to normal handler...")
+		return http.FileServer(http.Dir(set.Root))
+	}
+	dep, _ := genDeps(set.Root)
+
+	handler := &CAPHandler{
+		etags: ets,
+		deps:  dep,
+	}
+
+	return handler
 }
